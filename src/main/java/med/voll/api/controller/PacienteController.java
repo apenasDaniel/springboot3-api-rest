2c1d28327066cd45d8fcf450a16a20d3b9f217c5
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-11-17 09:56:31 -0300

    Produção de dados na API, Paginação e Ordenação

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,9 +2,13 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +25,9 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao ){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
